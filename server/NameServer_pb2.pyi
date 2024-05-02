6ed689ef5553c0508acf399b928236a2546b026b
Sykos7 <ivangarciapallares@gmail.com>
2024-05-02 15:19:57 +0200

    Test 5

---
+++ server/NameServer_pb2.pyi
@@ -0,0 +1,45 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class nothing(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
+class testResponse(_message.Message):
+    __slots__ = ("mesnsaje",)
+    MESNSAJE_FIELD_NUMBER: _ClassVar[int]
+    mesnsaje: str
+    def __init__(self, mesnsaje: _Optional[str] = ...) -> None: ...
+
+class GetChatParametersRequest(_message.Message):
+    __slots__ = ("chat_id",)
+    CHAT_ID_FIELD_NUMBER: _ClassVar[int]
+    chat_id: str
+    def __init__(self, chat_id: _Optional[str] = ...) -> None: ...
+
+class GetChatParametersResponse(_message.Message):
+    __slots__ = ("address", "port")
+    ADDRESS_FIELD_NUMBER: _ClassVar[int]
+    PORT_FIELD_NUMBER: _ClassVar[int]
+    address: str
+    port: int
+    def __init__(self, address: _Optional[str] = ..., port: _Optional[int] = ...) -> None: ...
+
+class RegisterClientRequest(_message.Message):
+    __slots__ = ("username", "ip_address", "port")
+    USERNAME_FIELD_NUMBER: _ClassVar[int]
+    IP_ADDRESS_FIELD_NUMBER: _ClassVar[int]
+    PORT_FIELD_NUMBER: _ClassVar[int]
+    username: str
+    ip_address: str
+    port: int
+    def __init__(self, username: _Optional[str] = ..., ip_address: _Optional[str] = ..., port: _Optional[int] = ...) -> None: ...
+
+class RegisterClientResponse(_message.Message):
+    __slots__ = ("success",)
+    SUCCESS_FIELD_NUMBER: _ClassVar[int]
+    success: bool
+    def __init__(self, success: bool = ...) -> None: ...
--- server/client.py
+++ server/client.py
@@ -19,7 +19,7 @@ import NameServer_pb2_grpc, NameServer_pb2
 
 channel = grpc.insecure_channel('localhost:50051')
 stub = NameServer_pb2_grpc.NameServerServiceStub(channel)
-response = stub.test()
+response = stub.test
 print(response.value)
 
 username = input("Introduce your username: ")
